99302f1d56d305a04db0d436102c15d7d3ebc8b5
Harish Soni <soniharish029@gmail.com>
2026-03-29 01:59:41 +0530

    Change API proxy target to production server

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:"http://localhost:3000",
+        target:"https://foresite-1.onrender.com/",
         changeOrigin:true,
         secure:false, 
       }
