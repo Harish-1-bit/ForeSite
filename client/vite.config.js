5ca7a33db6a12c594256e7b6d435b854218318db
Harish Soni <soniharish029@gmail.com>
2026-03-29 20:58:30 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:"http://127.0.0.1:3000",
+        target:"https://foresite-1.onrender.com",
         changeOrigin:true,
         secure:false, 
       }
